af799f77cae9979f26cb0f45f2a5021984caaca3
gfyffe <gfyffe@apple.com>
2023-09-14 11:35:35 -0700

    fix whitespace

--- gfwx.h
+++ gfwx.h
@@ -28,10 +28,10 @@
 //  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 //
 //  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in
-//	 the documentation and/or other materials provided with the distribution.
+//     the documentation and/or other materials provided with the distribution.
 //
 //  3. Neither the name of the organization nor the names of its contributors may be used to endorse or promote products derived from
-//	 this software without specific prior written permission.
+//     this software without specific prior written permission.
 //
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 //  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
